fba2fb58d5a0a1691e75aef548adc885d0330fe5
MZO9400 <hamza.hameed00@yahoo.com>
2017-03-25 09:09:21 +0100

    AOSPify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,14 +160,7 @@ TARGET_NO_RPC := true
 # Sensors
 USE_SENSOR_MULTI_HAL := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-   endif
-  endif
-endif
+      WITH_DEXPREOPT := false
 
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
